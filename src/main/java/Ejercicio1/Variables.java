487c67c12058ab7c92108e15cbb1a8e04fc550de
juan <juan@192.168.0.92>
2021-04-16 21:39:39 -0300

    Ejercicio 1 "Resuelto"

---
+++ src/main/java/Ejercicio1/Variables.java
@@ -0,0 +1,30 @@
+/*
+ Probar realizar las operaciones que se citan a continuación en el IDE Netbeans, registrar
+los errores obtenidos y responder a las preguntas citadas.
+a. Declarar una variable de tipo entero, otra de tipo double y otra de tipo String.
+b. Asignar un valor a la variable de tipo entero y asignar esta última a la variable de tipo
+double. ¿Existe un error? ¿Se puede hacer esta operación?
+c. Asignar un valor a la variable de tipo double y asignar esta última a la variable de tipo
+entero. ¿Existe un error? ¿Se puede hacer esta operación?
+d. Asignar el valor 35 a la variable de tipo String. ¿Existe un error? ¿Se puede hacer esta
+operación?
+e. Si quiero almacenar el número 48 en la variable String. ¿Cómo debo realizar la
+asignación
+ */
+package Ejercicio1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int variableEntero;
+        double variableDouble;
+        String variableString;
+        variableEntero = 10;
+        variableDouble = variableEntero;
+        variableDouble = 10.9;
+       // variableEntero = variableDouble;
+        
+        
+      //  variableString = 35;
+        variableString = "48";
+    }
+}
